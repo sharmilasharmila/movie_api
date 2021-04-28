06a7aa9f6261443ecf097137f2e7195aae651950
Sharmila Ratnasingam <sra084@uit.no>
2021-04-28 07:54:49 +0200

    Add file.txt

--- os_module.js
+++ os_module.js
@@ -1,9 +1,9 @@
-const os = require('os');
-const fs = require('fs');
+const os = require('os');// Built-in module to check about the Operating system.
+// This module used to get information about the OS
+const fs = require('fs');//Built -in module to do file operations (read and write)
 
-console.log(os.type());
+console.log(os.type());//Call the type function
 fs.readFile('file.txt','utf-8', (err,data) => {
     if (err) throw err;
     console.log(data);
 });
-
