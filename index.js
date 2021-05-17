0064e83d5920dde3b754d139b1c94d3c73210995
sharmilasharmila <sra084@post.uit.no>
2021-05-17 07:36:26 +0530

    CORS

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let auth = require('./auth')(app);
 require ('./passport');
 app.use(morgan('common'));
 
-let allowedOrigins = let allowedOrigins = ['http://localhost:8080', 'http://testsite.com',
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com',
   'https://sharmilamovie.herokuapp.com','http://localhost:1234','http://localhost:4565'];
 app.use(cors({
   origin: (origin, callback)=>{
