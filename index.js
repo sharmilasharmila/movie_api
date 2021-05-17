ab06c15c788c76dafa159c5af41859e1080b1637
sharmilasharmila <sra084@post.uit.no>
2021-05-17 07:34:55 +0530

    CORS

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.use(cors({
     return callback(null, true);
   }
 }));
-const Movies = Models.Movie;
-const Users = Models.User;
+// const Movies = Models.Movie;
+// const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
